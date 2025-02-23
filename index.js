aa7a485dd70d068a0fe0017f848a40f85c04c574
Abderrehman Boubnan <a.boubnan@outlook.com>
2025-02-23 19:49:20 +0100

    header fix et qui passe au noir quand on scroll

--- index.js
+++ index.js
@@ -4,3 +4,13 @@ function toggleMenu() {
     menu.classList.toggle('active');
 }
 
+window.addEventListener("scroll", function() {
+    const header = document.querySelector("header");
+    // Si on défile plus de 50px, on passe le header en noir, sinon on rétablit le style initial
+    if (window.scrollY > 50) {
+      header.style.backgroundColor = "#000";
+    } else {
+      header.style.backgroundColor = "rgba(0, 0, 0, 0.6)";
+    }
+  });
+
